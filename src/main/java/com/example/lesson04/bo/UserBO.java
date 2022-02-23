800d19c78473f36c0b68798055424ba9322c583a
DESKTOP-43O7LPO\baekj <baekjs0123@naver.com>
2022-02-24 01:01:30 +0900

    lesson04 ex01까지

---
+++ src/main/java/com/example/lesson04/bo/UserBO.java
@@ -0,0 +1,18 @@
+package com.example.lesson04.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.lesson04.dao.UserDAO;
+import com.example.lesson04.model.User;
+
+@Service
+public class UserBO {
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	public void addUser(String name, String yyyymmdd, String email, String introduce) {
+		User user = userDAO.insertUser(name, yyyymmdd, email, introduce);
+	}
+}
